7f1630916cecf3040543fa1647651de0ba0c5273
RajHiremath <hraj50@yahoo.com>
2016-02-01 23:12:32 -0800

    Added whatsapp msg send API

--- app/src/main/java/com/example/samyukta/myapplication/GeofenceTransitionsIntentService.java
+++ app/src/main/java/com/example/samyukta/myapplication/GeofenceTransitionsIntentService.java
@@ -3,6 +3,7 @@ package com.example.samyukta.myapplication;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
+import android.net.Uri;
 import android.telephony.SmsManager;
 import android.util.Log;
 
@@ -51,6 +52,7 @@ public class GeofenceTransitionsIntentService extends IntentService {
             String number = "4085054870";
             Log.d(TAG,"SomeMessage");
             //sm.sendTextMessage(number, null, "Picked up Vibusha", null, null);
+            //SendMessageToWhatsApp();
             //Send notification here
         } else {
             // Log the error.
@@ -58,5 +60,27 @@ public class GeofenceTransitionsIntentService extends IntentService {
         }
     }
 
+    private void SendMessageToWhatsApp() {
+        String phoneNumber = "+14085054890";
+        Uri uri = Uri.parse("smsto:" + phoneNumber);
+        Intent whatsAppIntent = new Intent(Intent.ACTION_SENDTO, uri);
+        //Intent whatsAppIntent = new Intent(Intent.ACTION_SEND);
 
+        Log.d(TAG, "SendMessageToWhatsApp: " + uri);
+        //whatsAppIntent.setType("text/plain");
+        whatsAppIntent.setType("text/plain");
+
+        String whatsApptext = "Sending message from Android App";
+        whatsAppIntent.setPackage("com.whatsapp");
+
+        Log.d(TAG, "SendMessageToWhatsApp: 2" + whatsApptext);
+
+        if (whatsAppIntent != null) {
+            Log.d(TAG, "Sendingmessage to whatapp: ");
+            whatsAppIntent.putExtra(whatsAppIntent.EXTRA_TEXT, whatsApptext);
+            startActivity(Intent.createChooser(whatsAppIntent, ""));
+            //startActivity(whatsAppIntent);
+        }
+
+    }
 }
